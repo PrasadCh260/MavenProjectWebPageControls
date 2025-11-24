78a244843e22b0f8c7210951540c417dff334476
Prasad ch <naidu.ch260@gmail.com>
2025-11-25 00:39:43 +0530

    code change

--- src/test/java/WebPageControls/practice2.java
+++ src/test/java/WebPageControls/practice2.java
@@ -3,7 +3,14 @@ package WebPageControls;
 public class practice2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String name="prasad";
+		char[] c=name.toCharArray();
+		
+		String reversedName="";
+		for(int i=c.length-1; i>=0; i--) {
+			reversedName+=c[i];
+		}
+		System.out.println(reversedName);
 
 	}
 
